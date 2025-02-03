180992d4208aa0044947e6afc515979e03a4b956
99JIWONDEV <99jiwonpark@gmail.com>
2025-02-03 13:45:01 +0900

    section03 대수타입

--- section03/src/chapter4.ts
+++ section03/src/chapter4.ts
@@ -13,3 +13,47 @@ a = true;
 
 let arr: (number | string | boolean)[] = [1, "hello", true];
 
+
+
+type Union1 = Dog | Person;
+
+let union1: Union1 = {
+  name: "",
+  color: ""
+}
+
+let union2 : Union1 = {
+  name: "",
+  language: ""
+}
+
+let union3 : Union1 = {
+  name: "",
+  color: "",
+  language: "",
+}
+
+// let union4 : Union1 = {
+//   name: "",
+// }
+
+// 교집합 타입 (Intersection Type)
+let variable : number & string; // never 공집합
+
+type Dog = {
+  name : string;
+  color: string; 
+}
+
+type Person = {
+  name : string;
+  language: string; 
+}
+
+type Intersection = Dog & Person;
+
+let intersection1 : Intersection = {
+  name: "",
+  color: "",
+  language: "",
+}
