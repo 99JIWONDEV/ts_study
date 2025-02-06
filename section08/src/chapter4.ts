ea437bfcac18fba0558e90d9501f03d8e10bb101
99JIWONDEV <99jiwonpark@gmail.com>
2025-02-06 21:39:43 +0900

    section08 템플릿 리터럴 타입

---
+++ section08/src/chapter4.ts
@@ -0,0 +1,8 @@
+// 템플릿 리터럴 타입 
+
+type Color = "red" | "blue" | "green";
+type Animal = "dog" | "cat" | "monkey";
+type ColoredAnimal = `${Color}-${Animal}`;
+
+
+const coloredAnimal: ColoredAnimal = "red-dog";
